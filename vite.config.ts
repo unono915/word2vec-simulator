f460b17c22d095e29dbfa873a8c2e6837b85654a
Yoonho Ji <76832553+unono915@users.noreply.github.com>
2025-06-05 12:45:51 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/word2vec-simulator/',
+  // 기타 설정
+});
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
